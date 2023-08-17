84b41fb95bd2e3f32ad22c19872f7b845a7153df
Harmanpreet Singh <harmanp397@gmail.com>
2023-08-17 13:25:33 +0530

    guthib auth error

--- src/server/auth.ts
+++ src/server/auth.ts
@@ -5,7 +5,7 @@ import {
   type NextAuthOptions,
   type DefaultSession,
 } from "next-auth";
-import GITHUBProvider from "next-auth/providers/GITHUB";
+import GITHUBProvider from "next-auth/providers/github"
 import { env } from "~/env.mjs";
 import { prisma } from "~/server/db";
 
